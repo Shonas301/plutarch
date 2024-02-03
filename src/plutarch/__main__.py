d4e35e4cebddee0afe67e8024036b1c2cd51bbbd
Jason Shipp <jason.s.shipp@gmail.com>
2024-02-03 14:24:41 -0500

    Make bot start with python module call

--- src/plutarch/__main__.py
+++ src/plutarch/__main__.py
@@ -1,2 +1,17 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# Initialize client
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(intents=intents, command_prefix='%')
+
+# Initialize env
+load_dotenv()
+
 if __name__ == "__main__":
-    print("Started")
+    print("Starting Plutarch")
+    client.run(os.getenv('DISCORD_TOKEN'))
